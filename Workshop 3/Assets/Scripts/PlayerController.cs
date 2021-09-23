f6670189606c30fc2ce7af6311ee1fac96724591
Peiwand Ismaiel <P.Ismaiel@student.han.nl>
2021-09-23 11:21:20 +0200

    Player movement

---
+++ Workshop 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 4;
+
+    private Rigidbody rb;
+    Vector3 pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void OnMove()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            pos = hit.point;
+            Debug.Log(pos);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+    }
+}
